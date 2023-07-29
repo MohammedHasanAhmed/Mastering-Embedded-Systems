60c2f6eb6952775b20366e6d999a783845a23f00
Mohammed Hasan <mohasanbder.com>
2023-07-29 14:16:50 +0300

    EX8 ==>> Assignment 2 (Simple Calculator)

--- Unit 2/Lesson 3.C_ basics/Homework 2/Code/EX8/EX8.c
+++ Unit 2/Lesson 3.C_ basics/Homework 2/Code/EX8/EX8.c
@@ -1,4 +1,4 @@
-//C Program to Make a Simple Calculator to Add, Subtract, Multiply or Divide Using switch...case
+//C Program to Make a Simple Calculator to Add, Subtract, Multiply or Divide Using switch...case 
 #include <stdio.h>
 
 int main(){
