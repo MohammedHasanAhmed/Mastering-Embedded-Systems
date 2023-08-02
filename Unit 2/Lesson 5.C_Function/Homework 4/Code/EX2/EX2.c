486edcfb6d56fb7d3f187433acc2633ce3a3bd52
Mohammed Hasan <mohasanbder.com>
2023-08-02 14:50:04 +0300

    EX2 ==>> Assignment 4 (Calculate Factorial of a Number)

--- Unit 2/Lesson 5.C_Function/Homework 4/Code/EX2/EX2.c
+++ Unit 2/Lesson 5.C_Function/Homework 4/Code/EX2/EX2.c
@@ -7,8 +7,8 @@
 
 #include <stdio.h>
 
-int factorial(int num);
-
+int factorial(int num); //function Using Recursion
+int fact(int num);     //function Using for loop
 int main()
 {
 	int num;
@@ -17,15 +17,28 @@ int main()
 	fflush(stdin);fflush(stdout);
 	scanf("%d" , &num);
 
-	printf("Factorial of %d = %d",num ,factorial(num));
+	printf("=====================\n");
+	printf("Factorial of %d = %d\n",num ,factorial(num)); // Using Recursion
+	printf("=====================\n");
+	printf("Factorial of %d = %d\n",num ,fact(num));     // Using for loop
 
 	return 0;
 }
-
+/*********************************************/
 int factorial(int num)
 {
 	if(num == 0)
 		return 1;
 	return num * factorial(num - 1);
 }
-
+/*********************************************/
+int fact(int num)
+{
+	int f = 1;
+	for( ; num>0 ; num--)
+	{
+		f*=num ;
+	}
+	return f;
+}
+/*********************************************/
