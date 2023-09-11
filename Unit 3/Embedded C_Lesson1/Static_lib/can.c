7c9e222ebf50323d0066076fe1d444778ff5d050
Mohammed Hasan <mohasanbder.com>
2023-09-11 22:28:06 +0300

    Static_lib ==>> Embedded C_Lesson1

---
+++ Unit 3/Embedded C_Lesson1/Static_lib/can.c
@@ -0,0 +1,6 @@
+#include "can.h"
+
+void can_init(char *ptr)
+{
+	printf("%s" , ptr);
+}
---
+++ Unit 3/Embedded C_Lesson1/Static_lib/main.c
@@ -0,0 +1,6 @@
+#include "can.h"
+
+void main(void)
+{
+	can_init("make *Static_lib* (Learn-In-Depth).\n");
+}
